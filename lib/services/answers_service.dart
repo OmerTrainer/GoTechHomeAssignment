eb980a1f34214b35474c84398adf38ae01030e2c
OmerTrainer <omertrainer3402@gmail.com>
2023-06-17 13:14:08 +0300

    restarting app when submitting form not ideal

--- lib/services/answers_service.dart
+++ lib/services/answers_service.dart
@@ -2,16 +2,21 @@ import 'dart:convert';
 import 'dart:math';
 
 import 'package:http/http.dart' as http;
+import 'package:restart_app/restart_app.dart';
 
 class AnswersService {
   static const String baseUrl = 'http://10.0.2.2:3000/answers';
 
   static void saveAllAnswers(List<String> answers) async {
-    await http.post(Uri.parse(baseUrl),
-        headers: <String, String>{
-          'Content-Type': 'application/json',
-        },
-        body:
-            jsonEncode({"id": Random().nextInt(10000000), "answers": answers}));
+    await http
+        .post(Uri.parse(baseUrl),
+            headers: <String, String>{
+              'Content-Type': 'application/json',
+            },
+            body: jsonEncode(
+                {"id": Random().nextInt(10000000), "answers": answers}))
+        .then((value) {
+      Restart.restartApp();
+    });
   }
 }
